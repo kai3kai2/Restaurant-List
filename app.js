2893167337903e757ba0935256c4ebee1f719b2d
kai3kai2 <111210838+kai3kai2@users.noreply.github.com>
2023-02-08 17:07:49 +0800

    feat: add session & passport & user login & user logout and authenticator in routers

--- app.js
+++ app.js
@@ -1,26 +1,38 @@
 // load express
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const session = require("express-session");
+const usePassport = require("./config/passport");
+const app = express();
+const port = 3000;
 
 // load handlebars
-const exphbs = require('express-handlebars')
-const methodOverride = require('method-override')
+const exphbs = require("express-handlebars");
+const methodOverride = require("method-override");
 
-const routes = require('./routes')
-require('./config/mongoose')
+const routes = require("./routes");
+require("./config/mongoose");
 
 // set handlebars
-app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
-app.set('view engine', 'handlebars')
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+app.use(
+  session({
+    secret: "ThisIsMySecret",
+    resave: false,
+    saveUninitialized: true,
+  })
+);
 
 // get js/cs file path
-app.use(express.static('public'))
-app.use(express.urlencoded({ extended: true }))
-app.use(methodOverride('_method'))
-app.use(routes)
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
+
+usePassport(app);
+
+app.use(routes);
 
 // server listen
 app.listen(port, () => {
-  console.log(`The server is listening on http://localhost:${3000}`)
-})
+  console.log(`The server is listening on http://localhost:${3000}`);
+});
--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,11 @@ const express = require("express");
 const router = express.Router();
 const home = require("./modules/home");
 const restaurants = require("./modules/restaurants");
-const users = require("./modules/user");
+const users = require("./modules/users");
+const { authenticator } = require("../middleware/auth");
 
-router.use("/", home);
+router.use("/restaurants", authenticator, restaurants);
 router.use("/users", users);
-router.use("/restaurants", restaurants);
+router.use("/", authenticator, home);
 
 module.exports = router;
