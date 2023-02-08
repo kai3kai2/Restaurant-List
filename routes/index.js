367df799519c3d389e91e0ecae0428fc2567795f
kai3kai2 <111210838+kai3kai2@users.noreply.github.com>
2023-02-08 15:02:30 +0800

    feat: add login and register page

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
-const express = require('express')
-const router = express.Router()
-const home = require('./modules/home')
-const restaurants = require('./modules/restaurants')
+const express = require("express");
+const router = express.Router();
+const home = require("./modules/home");
+const restaurants = require("./modules/restaurants");
+const users = require("./modules/user");
 
-router.use('/', home)
-router.use('/restaurants', restaurants)
+router.use("/", home);
+router.use("/users", users);
+router.use("/restaurants", restaurants);
 
-module.exports = router
+module.exports = router;
